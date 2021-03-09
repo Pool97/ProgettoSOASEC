9ffb96631ae85c4257ef3de14d92450c9d699faf
Pool97 <roberto.poletti01@gmail.com>
2021-03-09 17:23:35 +0100

    Creazione progetto Spring

---
+++ src/main/java/com/ProgettoSoasecApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSoasecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgettoSoasecApplication.class, args);
+    }
+
+}
