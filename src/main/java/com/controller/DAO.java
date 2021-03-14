7dce04108c7ca769bcfbfa3112d79a534f86c7e6
taba <matteo.tabarelli1997@gmail.com>
2021-03-14 19:23:16 +0100

    Quasi fatto

--- src/main/java/com/controller/DAO.java
+++ src/main/java/com/controller/DAO.java
@@ -1,8 +1,12 @@
 package com.controller;
 
+import com.model.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.util.List;
 
 /**
  * Un Data Access Object (DAO) per l'interfacciamento del server Java con il database MySQL.
@@ -21,7 +25,7 @@ public class DAO {
      * @param twoFAcode OTP generato per l'utente
      */
     public void update2FAProperties(String userID, String twoFAcode) {
-        jdbcTemplate.update("update users set 2fa_code=?, 2fa_expire_time=? where idUsers=?", twoFAcode, System.currentTimeMillis() / 1000 + 120, userID);
+        jdbcTemplate.update("UPDATE users SET 2fa_code=?, 2fa_expire_time=? WHERE idUsers=?", twoFAcode, System.currentTimeMillis() / 1000 + 120, userID);
     }
 
     /**
@@ -31,9 +35,28 @@ public class DAO {
      * @param twoFAcode OTP inserito dall'utente nella web-app
      * @return Validità del codice inserito dall'utente
      */
-    public boolean checkCode(String userID, String twoFAcode){
-        return jdbcTemplate.queryForObject("select count(*) from users where 2fa_code=?" +
-                "and idUsers = ? and 2fa_expire_time >=?", new Object[]{twoFAcode, userID , System.currentTimeMillis()/1000}, Integer.class) > 0;
+    public boolean checkCode(String userID, String twoFAcode) {
+        List<UserEntity> list = jdbcTemplate.query("SELECT COUNT(*) FROM users WHERE 2fa_code=? and email_id = ? and 2fa_expire_time >= ?", new Object[]{twoFAcode, userID, System.currentTimeMillis()/1000},
+                (ResultSet rs, int rowNum) -> {
+                    UserEntity user = new UserEntity();
+                    return user;
+                });
+        return list.size() > 0;
+    }
+
+    /**
+     * Verifica che l'userID dell'utente sia presente all'interno del database
+     * @param userID ID dell'utente nel database
+     * @return Presenza dell'utente nel database
+     */
+    public boolean checkUserID(String userID){
+        List<UserEntity> list = jdbcTemplate.query("SELECT COUNT(*) FROM users WHERE email_id = ?", new String[]{userID},
+                (ResultSet rs, int rowNum) -> {
+                    UserEntity user = new UserEntity();
+                    return user;
+                });
+        System.out.println(list.size());
+        return list.size() > 0;
     }
 
 }
